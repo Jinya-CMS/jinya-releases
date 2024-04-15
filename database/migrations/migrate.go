1e6ac10f94cf7ed68edb9626e14ab2e3d3b64505
Hans-Jürgen Wandschneider <juergen.wandschneider@jinya.de>
2024-04-15 16:59:25 +0200

    CHORE: Fix unit tests

--- database/migrations/migrate.go
+++ database/migrations/migrate.go
@@ -78,7 +78,7 @@ func Migrate() error {
 		return err
 	}
 
-	_ = db.MustExec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+	_, _ = db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
 
 	err = createMigrationsTable()
 	if err != nil {
