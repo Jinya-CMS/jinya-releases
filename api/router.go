844a5ca8f9e70bf5a0cdf47f7b3e5cc9edf56c14
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2024-06-30 03:17:52 +0200

    TG-30 Login via Zitadel
    Added Zitadel login for admin interface and api

--- api/router.go
+++ api/router.go
@@ -1,7 +1,16 @@
 package api
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"github.com/gorilla/mux"
+	"github.com/zitadel/oidc/v3/pkg/client"
+	"github.com/zitadel/zitadel-go/v3/pkg/authorization"
+	"github.com/zitadel/zitadel-go/v3/pkg/authorization/oauth"
+	"github.com/zitadel/zitadel-go/v3/pkg/http/middleware"
+	"github.com/zitadel/zitadel-go/v3/pkg/zitadel"
+	"jinya-releases/config"
 	"net/http"
 )
 
@@ -15,28 +24,50 @@ func contentTypeJson() func(next http.Handler) http.Handler {
 }
 
 func SetupApiRouter(router *mux.Router) {
-	router.Methods("GET").Path("/api/admin/application").Handler(contentTypeJson()(http.HandlerFunc(getAllApplications)))
-	router.Methods("POST").Path("/api/admin/application").Handler(contentTypeJson()(http.HandlerFunc(createApplication)))
-	router.Methods("GET").Path("/api/admin/application/{id}").Handler(contentTypeJson()(http.HandlerFunc(getApplicationById)))
-	router.Methods("PUT").Path("/api/admin/application/{id}").Handler(contentTypeJson()(http.HandlerFunc(updateApplication)))
-	router.Methods("DELETE").Path("/api/admin/application/{id}").Handler(contentTypeJson()(http.HandlerFunc(deleteApplication)))
-	router.Methods("POST").Path("/api/admin/application/{id}/logo").Handler(contentTypeJson()(http.HandlerFunc(uploadLogo)))
+	ctx := context.Background()
+	encryptionKey := make([]byte, 32)
+	_, err := rand.Read(encryptionKey)
+	if err != nil {
+		panic(err)
+	}
 
-	router.Methods("GET").Path("/api/admin/application/{applicationId}/track").Handler(contentTypeJson()(http.HandlerFunc(getAllTracks)))
-	router.Methods("POST").Path("/api/admin/application/{applicationId}/track").Handler(contentTypeJson()(http.HandlerFunc(createTrack)))
-	router.Methods("GET").Path("/api/admin/application/{applicationId}/track/{id}").Handler(contentTypeJson()(http.HandlerFunc(getTrackById)))
-	router.Methods("PUT").Path("/api/admin/application/{applicationId}/track/{id}").Handler(contentTypeJson()(http.HandlerFunc(updateTrack)))
-	router.Methods("DELETE").Path("/api/admin/application/{applicationId}/track/{id}").Handler(contentTypeJson()(http.HandlerFunc(deleteTrack)))
+	keyFileData, err := base64.StdEncoding.DecodeString(config.LoadedConfiguration.OpenIDKeyFileData)
+	if err != nil {
+		panic(err)
+	}
 
-	router.Methods("GET").Path("/api/admin/application/{applicationId}/track/{trackId}/version").Handler(contentTypeJson()(http.HandlerFunc(getAllVersions)))
-	router.Methods("POST").Path("/api/admin/application/{applicationId}/track/{trackId}/version").Handler(contentTypeJson()(http.HandlerFunc(createVersion)))
-	router.Methods("GET").Path("/api/admin/application/{applicationId}/track/{trackId}/version/{id}").Handler(contentTypeJson()(http.HandlerFunc(getVersionById)))
-	router.Methods("DELETE").Path("/api/admin/application/{applicationId}/track/{trackId}/version/{id}").Handler(contentTypeJson()(http.HandlerFunc(deleteVersion)))
-	router.Methods("POST").Path("/api/admin/application/{applicationId}/track/{trackId}/version/{id}/file").Handler(contentTypeJson()(http.HandlerFunc(uploadVersionBinary)))
+	keyFile, err := client.ConfigFromKeyFileData(keyFileData)
+	zitadelConfig := oauth.WithIntrospection[*oauth.IntrospectionContext](oauth.JWTProfileIntrospectionAuthentication(keyFile))
+	authZ, err := authorization.New(ctx, zitadel.New(config.LoadedConfiguration.OpenIDDomain), zitadelConfig)
 
-	router.Methods("GET").Path("/api/admin/push-token").Handler(contentTypeJson()(http.HandlerFunc(getAllPushTokens)))
-	router.Methods("POST").Path("/api/admin/push-token").Handler(contentTypeJson()(http.HandlerFunc(createPushToken)))
-	router.Methods("GET").Path("/api/admin/push-token/{id}").Handler(contentTypeJson()(http.HandlerFunc(getPushTokenById)))
-	router.Methods("PUT").Path("/api/admin/push-token/{id}").Handler(contentTypeJson()(http.HandlerFunc(updatePushToken)))
-	router.Methods("DELETE").Path("/api/admin/push-token/{id}").Handler(contentTypeJson()(http.HandlerFunc(deletePushToken)))
+	if err != nil {
+		panic(err)
+	}
+
+	mw := middleware.New(authZ)
+
+	router.Methods("GET").Path("/api/admin/application").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(getAllApplications))))
+	router.Methods("POST").Path("/api/admin/application").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(createApplication))))
+	router.Methods("GET").Path("/api/admin/application/{id}").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(getApplicationById))))
+	router.Methods("PUT").Path("/api/admin/application/{id}").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(updateApplication))))
+	router.Methods("DELETE").Path("/api/admin/application/{id}").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(deleteApplication))))
+	router.Methods("POST").Path("/api/admin/application/{id}/logo").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(uploadLogo))))
+
+	router.Methods("GET").Path("/api/admin/application/{applicationId}/track").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(getAllTracks))))
+	router.Methods("POST").Path("/api/admin/application/{applicationId}/track").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(createTrack))))
+	router.Methods("GET").Path("/api/admin/application/{applicationId}/track/{id}").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(getTrackById))))
+	router.Methods("PUT").Path("/api/admin/application/{applicationId}/track/{id}").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(updateTrack))))
+	router.Methods("DELETE").Path("/api/admin/application/{applicationId}/track/{id}").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(deleteTrack))))
+
+	router.Methods("GET").Path("/api/admin/application/{applicationId}/track/{trackId}/version").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(getAllVersions))))
+	router.Methods("POST").Path("/api/admin/application/{applicationId}/track/{trackId}/version").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(createVersion))))
+	router.Methods("GET").Path("/api/admin/application/{applicationId}/track/{trackId}/version/{id}").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(getVersionById))))
+	router.Methods("DELETE").Path("/api/admin/application/{applicationId}/track/{trackId}/version/{id}").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(deleteVersion))))
+	router.Methods("POST").Path("/api/admin/application/{applicationId}/track/{trackId}/version/{id}/file").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(uploadVersionBinary))))
+
+	router.Methods("GET").Path("/api/admin/push-token").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(getAllPushTokens))))
+	router.Methods("POST").Path("/api/admin/push-token").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(createPushToken))))
+	router.Methods("GET").Path("/api/admin/push-token/{id}").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(getPushTokenById))))
+	router.Methods("PUT").Path("/api/admin/push-token/{id}").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(updatePushToken))))
+	router.Methods("DELETE").Path("/api/admin/push-token/{id}").Handler(mw.RequireAuthorization()(contentTypeJson()(http.HandlerFunc(deletePushToken))))
 }
