ede94546cfeee0ef7f184f08254a867d72c16f88
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2024-07-02 02:08:40 +0200

    TG-34 Create version
    Added management for versions in the admin interface

--- angular/projects/api/src/lib/models/application.ts
+++ angular/projects/api/src/lib/models/application.ts
@@ -1,12 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
 export interface Application {
-  additionalCss?: string;
-  additionalJavaScript?: string;
-  homepageTemplate: string;
   id: string;
   logo?: string;
   name: string;
   slug: string;
-  trackpageTemplate: string;
 }
--- angular/projects/api/src/lib/models/update-application.ts
+++ angular/projects/api/src/lib/models/update-application.ts
@@ -1,10 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
 export interface UpdateApplication {
-  additionalCss?: string;
-  additionalJavaScript?: string;
-  homepageTemplate: string;
   name: string;
   slug: string;
-  trackpageTemplate: string;
 }
